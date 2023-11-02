d390c50ada8113ff888abf7cbedadc5f0c105cea
Yousef <yousefayman2000500@gmail.com>
2023-11-02 03:21:38 +0200

    conditonal content and a temporary way of updating state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editHandler = () => {
+    setIsEditing(!isEditing);
+  };
+
   return (
     <li>
-      <span>
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" required value={name} />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>edit</button>
+      <button onClick={editHandler}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
