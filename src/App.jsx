db130a5a9b72d6eed069816b12bd84ded0b58f1d
Yousef <yousefayman2000500@gmail.com>
2023-11-13 09:49:07 +0200

    lifting computed values up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
-
 import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 const derivedActivePlayer = (gameTurns) => {
   let currentPlayer = "X";
@@ -15,6 +20,13 @@ const derivedActivePlayer = (gameTurns) => {
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -41,7 +53,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
